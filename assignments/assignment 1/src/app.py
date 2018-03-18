99ae6de6f2bacf76fd89c81950878b158c776730
mourad1081 <mourad1081@gmail.com>
2018-03-18 20:08:22 +0100

    lol

--- assignments/assignment 1/src/app.py
+++ assignments/assignment 1/src/app.py
@@ -1,16 +1,21 @@
 from src.LanguageModel import LanguageModel, ANSI
 
 
-def train_and_export(path):
+def generate_language_model(path):
     print(ANSI.header, "Training", path, "corpus")
     print(" -------------------------------------------", ANSI.endc)
     lm = LanguageModel(path)
+
     lm.generate_trigrams_counts()
-    lm.generate_trigram_probabilities()
-    lm.add_one_smoothing()
+
+    k = 2 if path.endswith("AU") else 1 if path.endswith("GB") else 2
+    lm.add_k_smoothing(k=k)
+    lm.normalize_counts()
+
     lm.export()
-    print(lm.generate_random_output().replace("__", " ").replace("_", ""))
+    lm.generate_random_output(export_to_file=True)
     print("\n")
+    return lm
 
 
 """
@@ -25,5 +30,28 @@ if __name__ == '__main__':
 
     test_set = ["../test_set/test"]
 
+    # We create the LM's
+    language_models = []
     for corpus in training_set:
-        train_and_export(corpus)
+        language_models.append(generate_language_model(corpus))
+
+    # We classify the test set
+    for test in test_set:
+        file = open(test, 'r')
+        perplexities = {}
+        i = 0
+        for line in file.readlines():
+            perplexities['line ' + str(i)] = {}
+            print(ANSI.ok_blue, "Line to classify (results below):")
+            print(" ", line, ANSI.endc)
+            print(" ➤➤➤ Results:")
+            print(" -----------")
+            for lmodel in language_models:
+                perplexities['line ' + str(i)][lmodel.name] = lmodel.compute_perplexity(line)
+                print(" ➤➤➤ Perplexity of", lmodel.name, "=", perplexities['line ' + str(i)][lmodel.name])
+
+            x = min(perplexities['line ' + str(i)].keys(), key=(lambda k: perplexities['line ' + str(i)][k]))
+            print(ANSI.header, '★★★★★★★★★★★ Best result:', x, '★★★★★★★★★★★★', ANSI.endc)
+            i += 1
+
+        file.close()
