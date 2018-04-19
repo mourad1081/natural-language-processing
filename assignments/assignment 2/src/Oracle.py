cd0c8aa9fe77e8b6787535eb70a4446a166c9b1d
Mourad <mourad1081@gmail.com>
2018-04-19 23:39:58 +0200

    lol

---
+++ assignments/assignment 2/src/Oracle.py
@@ -0,0 +1,10 @@
+class Oracle:
+    def __init__(self):
+        self.reference_parse = []
+
+    def train(self, training_set):
+        pass
+
+    def pick_choice(self, configuration):
+        pass
+
---
+++ assignments/assignment 2/src/TransitionBasedDependyParser.py
@@ -0,0 +1,9 @@
+from . import Oracle.Oracle
+
+class TransitionBasedDependencyParser:
+    def __init__(self):
+        self.oracle = Oracle()
+
+
+    def parse(self):
+        pass
